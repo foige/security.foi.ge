1b5b11bca0230f3965ec39e6184f2907fc91c436
grapexy <100813784+grapexy@users.noreply.github.com>
2025-02-07 07:06:18 +0400

    feat(bl_tools): Add warnings if insecure encryption is used

--- docs/policies/files/windows/bl_tools.cmd
+++ docs/policies/files/windows/bl_tools.cmd
@@ -183,25 +183,35 @@ echo.
 echo  -------------------------------------------------------
 if defined is_encrypted (
     if defined is_encrypting (
-        echo    BitLocker encryption in progress. Monitoring progress...
-        echo    Press any key to return to menu
+        echo    BitLocker encryption in progress...
         echo  -------------------------------------------------------
         echo.
         
         :: Monitor encryption progress with fvenotify
         start /b fvenotify.exe !selected_drive!
-        pause > nul
-        goto menu
+        
+        echo    1. Back to menu  R. Refresh
+        echo  -------------------------------------------------------
+        echo.
+        set /p "action=Selection: "
+
+        if /i "!action!"=="R" goto drive_menu
+        if /i "!action!"=="1" goto menu
     ) else if defined is_decrypting (
-        echo    BitLocker decryption in progress. Monitoring progress...
-        echo    Press any key to return to menu
+        echo    BitLocker decryption in progress...
         echo  -------------------------------------------------------
         echo.
         
         :: Monitor decryption progress with fvenotify
         start /b fvenotify.exe !selected_drive!
-        pause > nul
-        goto menu
+        
+        echo    1. Back to menu  R. Refresh
+        echo  -------------------------------------------------------
+        echo.
+        set /p "action=Selection: "
+
+        if /i "!action!"=="R" goto drive_menu
+        if /i "!action!"=="1" goto menu
     ) else (
         echo    1. Disable BitLocker  2. Back to menu  R. Refresh
         echo  -------------------------------------------------------
